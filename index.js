57273d72dfdc129dabfb75a8659b195bb1bebed2
Spandan Mukherjee <95229740+SpandanM110@users.noreply.github.com>
2023-10-17 19:38:51 +0000

    changes

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ const fs = require('fs')
 const path = require('path')
 
 const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirects.yml'), 'utf-8')
+const redirects = YAML.parse(redirectsFile)
+
 console.log(redirectsFile)
 
