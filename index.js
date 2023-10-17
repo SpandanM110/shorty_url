f04f41b40caaf3229d36480a5f66864adda0d2ff
Spandan Mukherjee <95229740+SpandanM110@users.noreply.github.com>
2023-10-17 19:36:24 +0000

    index.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 // generate an html page for each redirect url from template.html
 
 const YAML = require('yaml')
+const fs = require('fs')
+const path = require('path')
+
+const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirects.yml'), 'utf-8')
+console.log(redirectsFile)
+
